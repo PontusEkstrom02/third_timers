9b969b37688a3db213cfbb29beec0673747c62c6
PontusEkstrom02 <113009598+PontusEkstrom02@users.noreply.github.com>
2023-05-29 13:35:58 +0200

    Update adminUserPage.js

--- client/src/pages/adminUserPage.js
+++ client/src/pages/adminUserPage.js
@@ -1,7 +1,7 @@
 /*This file will render an admin view with all available user categories a search bar, a sign out button and two buttons for promoting  or deleting a user.
 There will also be a tab for changing tables for user and book view.*/
-import BooksTableTab from "../components/BooksTableTab"
-import UserTableTab from "../components/UserTableTab"
+import BooksTableTab from "../components/booksTableTab"
+import UserTableTab from "../components/userTableTab"
 import UserTable from "../components/userTable"
 export default function adminUserPage() {
     return(
