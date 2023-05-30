6c864eb8feb287f580b536cafa160dddf672fd33
PontusEkstrom02 <113009598+PontusEkstrom02@users.noreply.github.com>
2023-05-30 18:05:36 +0200

    functional search in usertable and adminuserpage

--- client/src/components/userTable.js
+++ client/src/components/userTable.js
@@ -1,31 +1,43 @@
 import React, { useState, useEffect } from "react";
 import UserActionField from "./userActionField";
 
-export default function UserTable() {
+export default function UserTable({ search }) {
   const [userlist, setUserlist] = useState([]);
+  const [filteredUserlist, setFilteredUserlist] = useState([]);
 
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
 
   useEffect(() => {
-    fetch('http://localhost:3001/admin/users', {
-      method: 'GET',
+    fetch("http://localhost:3001/admin/users", {
+      method: "GET",
       headers: {
         Authorization: `Bearer ${token}`,
       },
     })
-    .then(response => response.json())
-    .then(data => {
-      setUserlist(data.users);
-    })
-    .catch(error => {
-      console.error(error);
-    });
+      .then((response) => response.json())
+      .then((data) => {
+        setUserlist(data.users);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }, []);
+
+  useEffect(() => {
+    if (search.trim() === "") {
+      setFilteredUserlist(userlist);
+    } else {
+      const filteredList = userlist.filter((user) =>
+        user.username.includes(search)
+      );
+      setFilteredUserlist(filteredList);
+    }
+  }, [search, userlist]);
 
   const calculateTotalPurchases = (purchases) => {
     let total = 0;
     if (Array.isArray(purchases)) {
-      purchases.forEach(purchase => {
+      purchases.forEach((purchase) => {
         total += purchase.quantity;
       });
     }
@@ -43,12 +55,15 @@ export default function UserTable() {
         </tr>
       </thead>
       <tbody>
-        {userlist.map((user) => (
+        {filteredUserlist.map((user) => (
           <tr key={user.id}>
             <td>{user.username}</td>
             <td>{user.role}</td>
-            <td>{calculateTotalPurchases(user.purchases) + " purcheses"}</td>
-            <UserActionField userId={user.username} setupdatedUserList={setUserlist}/>
+            <td>{calculateTotalPurchases(user.purchases) + " purchases"}</td>
+            <UserActionField
+              userId={user.username}
+              setupdatedUserList={setUserlist}
+            />
           </tr>
         ))}
       </tbody>
--- client/src/pages/adminUserPage.js
+++ client/src/pages/adminUserPage.js
@@ -1,14 +1,19 @@
 /*This file will render an admin view with all available user categories a search bar, a sign out button and two buttons for promoting  or deleting a user.
 There will also be a tab for changing tables for user and book view.*/
+import React, { useState } from 'react';
 import BooksTableTab from "../components/booksTableTab"
 import UserTableTab from "../components/userTableTab"
 import UserTable from "../components/userTable"
-export default function adminUserPage() {
+import SearchBar from "../components/searchBar"
+export default function AdminUserPage() {
+    const [search, setSearch] = useState('');
+
     return(
         <div>
+            <SearchBar placeholder="Search by username..." onChange={event => setSearch(event.target.value)}/>
             <BooksTableTab />
             <UserTableTab />
-            <UserTable />
+            <UserTable search={search}/>
         </div>
     )
 }
