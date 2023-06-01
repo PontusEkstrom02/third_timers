2c52b682293ce572d93e8fd61243709bd04e237a
MohamadAliHassan <113009645+MohamadAliHassan@users.noreply.github.com>
2023-06-01 20:34:02 +0200

    Update adminBookPage.js

--- client/src/pages/adminBookPage.js
+++ client/src/pages/adminBookPage.js
@@ -1,28 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import BooksTable from "../components/booksTable";
 import SearchBar from "../components/searchBar";
-import BooksTableTab from "../components/booksTableTab"
-import UserTableTab from "../components/userTableTab"
-import AddEditPopup from '../components/addEditPopUp';
+import BooksTableTab from "../components/booksTableTab";
+import UserTableTab from "../components/userTableTab";
+import AddEditPopup from "../components/addEditPopUp";
 
-import Headertab from '../components/headerTab'
+import Headertab from "../components/headerTab";
 function AdminBookPage() {
-  const [search, setSearch] = useState('');
-  
- 
+  const [search, setSearch] = useState("");
+
   return (
     <>
-      <Headertab role="ADMIN"/>
+      <Headertab role="ADMIN" />
       <main className="adminBookPage-container">
-        <SearchBar placeholder="Search query..." onChange={event => setSearch(event.target.value)} />
+        <SearchBar
+          placeholder="Search query..."
+          onChange={(event) => setSearch(event.target.value)}
+        />
         <AddEditPopup />
         <BooksTableTab />
         <UserTableTab />
-        <BooksTable search={search} role="admin"/>
+        <BooksTable search={search} role="admin" />
       </main>
     </>
   );
-
- }
+}
 export default AdminBookPage;
-
