567f69bfb76ebc07aad070f1cb3574704af15e3f
MohamadAliHassan <113009645+MohamadAliHassan@users.noreply.github.com>
2023-06-02 09:18:23 +0200

    Update userPage.js

--- client/src/pages/userPage.js
+++ client/src/pages/userPage.js
@@ -1,24 +1,22 @@
 //This file will render a user view with books and their information. The file will also render a search bar, a sign out button, increment/decrement buttons and an order button.
-import React, { useState } from 'react';
-import SearchBar from '../components/searchBar';
-import BooksTable from '../components/booksTable';
-import Headertab from '../components/headerTab'
+import React, { useState } from "react";
+import SearchBar from "../components/searchBar";
+import BooksTable from "../components/booksTable";
+import Headertab from "../components/headerTab";
 
 export default function UserPage() {
-  const [search, setSearch] = useState('');
-  
-  
+  const [search, setSearch] = useState("");
+
   return (
     <>
-      <Headertab role="USER"/>
+      <Headertab role="USER" />
       <main>
         <SearchBar
           placeholder="Search query..."
-          onChange={event => setSearch(event.target.value)}
+          onChange={(event) => setSearch(event.target.value)}
         />
-        <BooksTable search={search} role="user"/>
+        <BooksTable search={search} role="user" />
       </main>
     </>
   );
 }
-
